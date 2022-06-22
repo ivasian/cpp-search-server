0fad19bc5abd06dd0618f900994f7592c6b67f8e
Andrei MArchenko <ravshanolezhe@yandex.ru>
2022-06-22 15:33:51 +0300

    The final project for the sixth sprint

--- search-server/read_input_functions.h
+++
@@ -1,5 +0,0 @@
-#pragma once 
-#include <string>
-
-std::string ReadLine();
-int ReadLineWithNumber();
--- search-server/remove_duplicates.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "search_server.h"
-#include <set>
-#include <string>
-#include <map>
-
-void RemoveDuplicates(SearchServer& search_server);
